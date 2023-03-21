9d77f8228bfdaefbce1f2b246cccdb1acd3d87bc
Esteban Muñoz <estebanmunozrojas@gmail.com>
2023-03-21 17:16:14 -0300

    dep test

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import postRoutes from "./routes/post.routes.js";
-
+import {dirname,join} from 'path'
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
+console.log(__dirname)
 
 app.use(express.json())
 app.use(fileUpload({
@@ -11,4 +14,9 @@ app.use(fileUpload({
 }))
 app.use(postRoutes);
 
+app.use(express.static(join(__dirname,'../client/build')))
+
 export default app
+
+
+// pass vEbMBtbiPgKTYJTP
